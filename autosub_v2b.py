d3f11fc49f8bdc9ed6e86deab6a07daf5761858e
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-24 22:42:59 +0800

    Update autosub_v2b.py

--- autosub_v2b.py
+++ autosub_v2b.py
@@ -9,7 +9,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101
 
 home_urls = (
     'https://baipiao.buzz',
-    'v2.amaoaskworld.tk',
+    'https://v2.amaoaskworld.tk',
 )
 times = 15
 proxies = {
