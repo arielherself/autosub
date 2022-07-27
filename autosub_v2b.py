65a3d817a356188a57b7ccc4df929d5bc754625c
hkaa0 <94212566+hkaa0@users.noreply.github.com>
2022-07-28 00:38:05 +0800

    Update autosub_v2b.py

--- autosub_v2b.py
+++ autosub_v2b.py
@@ -9,7 +9,8 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101
 
 home_urls = (
     'https://baipiao.buzz',
-    'https://whk.life',
+    'http://whk.life',
+    'https://freecloud.lol',
 )
 times = 15
 proxies = {
